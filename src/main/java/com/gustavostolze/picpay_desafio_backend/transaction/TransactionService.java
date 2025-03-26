de459c0d4ccd43681cc8ca206e3fd09edbe6f2c0
Gustavo Stolze <gustavostolzedeandrade@gmail.com>
2025-03-25 23:18:20 -0300

    create transaction roadmap

---
+++ src/main/java/com/gustavostolze/picpay_desafio_backend/transaction/TransactionService.java
@@ -0,0 +1,22 @@
+package com.gustavostolze.picpay_desafio_backend.transaction;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+	public Transaction create(Transaction transaction) {
+		// validate transaction
+		
+		// save transaction
+		
+		// debit and credit wallets
+		
+		// call external services
+			// authorize
+		
+			// notification
+		
+		return null;
+	}
+}
